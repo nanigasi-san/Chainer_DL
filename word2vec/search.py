7812b56e2ca5026a14287b3426a2586acd7e4edf
nanigasi <nanigasi.py@gmail.com>
2019-03-19 02:28:40 +0900

    脱出機能を搭載

--- word2vec/search.py
+++ word2vec/search.py
@@ -25,6 +25,8 @@ w /= s.reshape((s.shape[0], 1))  # normalize
 try:
     while True:
         q = six.moves.input('>> ')
+        if q == "exit":
+            break
         if q not in word2index:
             print('"{0}" is not found'.format(q))
             continue
